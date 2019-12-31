498a3a28b038f72d92f5643f686224add4f02f56
Michael Nathan <realmichaelnathan@protonmail.com>
2019-12-30 20:53:56 -0600

    linking to new css on layout.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,7 +19,7 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz"
 	 crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat|Luckiest+Guy&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="{{ asset('css/custom.css') }}" /> 
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}" /> 
 	@yield('headers')
 </head>
 
